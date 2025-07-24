1f670e4f4eb275372833c51f5733efd560fe3be6
Krushna Yerawar <krushnayerawar2004@gmail.com>
2025-07-24 09:06:43 +0530

    relational operators

---
+++ relational operators/Relational_Operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+
+int main (){
+int a = 55 ;
+int b = 88 ;
+
+cout << "operation == " << ( a == b ) << endl ;
+cout << "operation > " << ( a > b ) << endl ;
+cout << "operation >= " << ( a >= b ) << endl ;
+cout << "operation <= " << ( a <= b ) << endl ;
+cout << "operation != " << ( a != b ) << endl ;
+return 0 ;
+}
+
