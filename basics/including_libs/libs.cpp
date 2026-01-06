726da60e3a5d0470bae4f4bde7ea273af63cb19f
KPYerawar <you@example.com>
2026-01-06 19:42:36 +0530

    some printing questions tried

---
+++ basics/including_libs/libs.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main (){
+// iostream = essential for input and outputs 
+//math.h for mathematical questions
+return 0; }
---
+++ basics/printing/pointer_arthmatic_on_string_litrals.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout << "krushna" +5 << endl;
+cout << "krushna" +0 << endl;
+cout<< "my name is krushna" + 4 <<endl;
+cout << "hello"5<<  "world" << endl;
+return 0 ;
+}
